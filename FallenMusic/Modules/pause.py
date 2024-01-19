601508ee2333eaac7170e01dc37daa100ea0cca8
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-19 07:01:51 +0700

    Update pause.py

--- FallenMusic/Modules/pause.py
+++ FallenMusic/Modules/pause.py
@@ -37,12 +37,12 @@ async def pause_str(_, message: Message):
 
     if not await is_streaming(message.chat.id):
         return await message.reply_text(
-            "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ʀᴇsᴜᴍᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
+            "ᴀᴘᴀᴋᴀʜ ᴀɴᴅᴀ ɪɴɢɪɴ ᴍᴇʟᴀɴᴊᴜᴛᴋᴀɴ ꜱᴛʀᴇᴀᴍɪɴɢ ?"
         )
 
     await pytgcalls.pause_stream(message.chat.id)
     await stream_off(message.chat.id)
     return await message.reply_text(
-        text=f"➻ sᴛʀᴇᴀᴍ ᴩᴀᴜsᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        text=f"‣ ꜱᴛʀᴇᴀᴍɪɴɢ ᴅɪᴊᴇᴅᴀ\n│ \n└ᴏʟᴇʜ : {message.from_user.mention}",
         reply_markup=close_key,
     )
