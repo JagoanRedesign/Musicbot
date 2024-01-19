58a4140efe48a93ee62b7cb33f44c34f9c70613f
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-19 12:16:07 +0700

    Update assistant.py

--- FallenMusic/Modules/assistant.py
+++ FallenMusic/Modules/assistant.py
@@ -1,5 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram import Client
 
 from FallenMusic import ASS_MENTION, LOGGER, SUDOERS, app, app2
 
@@ -78,7 +79,7 @@ async def set_name(_, message: Message):
 
 
 TARGET = -1001651683956
-@app.on_message(filters.chat(TARGET) & filters.new_chat_members)
+@Client.on_message(filters.chat(TARGET) & filters.new_chat_members)
 async def welcome(client, message):
    text = f"Halo {message.new_chat_members[0].first_name}, dan selamat datang di {message.chat.title}!"
      await app2.reply_text(text, disable_web_page_preview=True)
